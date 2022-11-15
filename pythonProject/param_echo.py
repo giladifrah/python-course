1f849e25b0c69818c60dcdc92897590a18c4016c
Gilad Ifrah <gilad.ifrah@gmail.com>
2022-11-15 17:25:21 +0200

    Python 10.11.2022

--- pythonProject/param_echo.py
+++ pythonProject/param_echo.py
@@ -5,3 +5,5 @@ print(f"First argument:{sys.argv[0]}")
 print(f"Second argument:{sys.argv[1]}")
 print(f"Third argument:{sys.argv[2]}")
 print(f"Fourth argument:{sys.argv[3]}")
+
+print(f"All argument:{sys.argv[1:]}")
